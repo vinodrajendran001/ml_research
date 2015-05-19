f07668e1fcae1cd40ae72be3a34c476af3864d2a
Christopher Collins <crcoll9955@northgeorgia.edu>
2015-05-19 13:44:33 -0400

    Change the data directory path into a constant

--- load_data.py
+++ load_data.py
@@ -5,6 +5,9 @@ from utils import tokenize, ARYL, true_strip, erf_over_r, read_file_data, \
          map_atom
 
 
+DATA_BASE_DIR = "data"
+
+
 def load_mol_data(calc_set, opt_set, struct_set, prop_set=None):
     '''
     Load data from data sets and return lists of structure names, full paths
@@ -25,7 +28,7 @@ def load_mol_data(calc_set, opt_set, struct_set, prop_set=None):
     for j, base_path in enumerate(opt_set):
         for i, file_path in enumerate(calc_set):
             for m, atom_set in enumerate(struct_set):
-                path = os.path.join('../mol_data', base_path, atom_set, file_path)
+                path = os.path.join(DATA_BASE_DIR, "mol_data", base_path, atom_set, file_path)
                 with open(path + ".txt", 'r') as f:
                     for line in f:
                         temp = line.split()
@@ -34,7 +37,7 @@ def load_mol_data(calc_set, opt_set, struct_set, prop_set=None):
                         names.append(name)
                         datasets.append((base_path, file_path, atom_set))
 
-                        geom_path = os.path.join('../mol_data', base_path, 'geoms', 'out', name + '.out')
+                        geom_path = os.path.join(DATA_BASE_DIR, "mol_data", base_path, 'geoms', 'out', name + '.out')
                         geom_paths.append(geom_path)
 
                         properties.append([float(x) for x in props])
@@ -58,7 +61,7 @@ def load_mol_data(calc_set, opt_set, struct_set, prop_set=None):
 def build_gdb7_data():
     BHOR_TO_ANGSTROM = 0.529177249
     atoms = {1: 'H', 6: "C", 7: "N", 8: "O", 16: "S"}
-    with open("qm7.pkl", "r") as f:
+    with open(os.path.join(DATA_BASE_DIR, "qm7.pkl"), "r") as f:
         temp = cPickle.load(f)
         X = temp['X'].reshape(7165, 23*23)
         Z = temp['Z']
@@ -71,7 +74,7 @@ def build_gdb7_data():
             continue
 
         name = "qm-%04d" % i
-        path = "qm/" + name + ".out"
+        path = os.path.join(DATA_BASE_DIR, "qm" + name + ".out")
         with open(path, "w") as f:
             for z, coord in zip(zs, coords):
                 z = int(z)
@@ -79,9 +82,10 @@ def build_gdb7_data():
                     coord = [x * BHOR_TO_ANGSTROM for x in coord]
                     f.write("%s %.8f %.8f %.8f\n" % (atoms[z], coord[0], coord[1], coord[2]))
 
+
 def load_gb7_data():
-    if not os.path.isdir("data/gdb7") or not os.listdir("data/gdb7"):
-        build_gdb7()
+    if not os.path.isdir(os.path.join(DATA_BASE_DIR, "gdb7")) or not os.listdir(os.path.join(DATA_BASE_DIR, "gdb7")):
+        build_gdb7_data()
 
     names = []
     datasets = []
@@ -90,7 +94,7 @@ def load_gb7_data():
     meta = []
     lengths = []
 
-    with open("data/qm7.pkl", "r") as f:
+    with open(os.path.join(DATA_BASE_DIR, "qm7.pkl"), "r") as f:
         temp = cPickle.load(f)
         X = temp['X'].reshape(7165, 23*23)
         Z = temp['Z']
@@ -100,7 +104,7 @@ def load_gb7_data():
 
     for i, (zs, coords, t) in enumerate(zip(Z, R, T)):
         name = "qm-%04d" % i
-        path = "data/qm/" + name + ".out"
+        path = os.path.join(DATA_BASE_DIR, "qm", name + ".out")
         names.append(name)
         datasets.append((1, ))
         geom_paths.append(path)
@@ -112,7 +116,7 @@ def load_gb7_data():
 
 def build_dave_data():
     atoms = {"1": 'H', "6": "C", "7": "N", "8": "O", "9": "F"}
-    with open("data/geom.txt", 'r') as f:
+    with open(os.path.join(DATA_BASE_DIR, "geom.txt"), 'r') as f:
         elements = []
         for line in f:
             numbers = line.strip().split()
@@ -122,7 +126,7 @@ def build_dave_data():
                 except UnboundLocalError:
                     pass
                 name = "dave-%04d" % (int(numbers[0]) - 1)
-                path = "dave/" + name + ".out"
+                path = os.path.join(DATA_BASE_DIR, "dave", name + ".out")
                 f2  = open(path, "w")
             elif len(numbers) == 3:
                 f2.write(" ".join([elements.pop()] + numbers) + "\n")
@@ -131,8 +135,8 @@ def build_dave_data():
 
 
 def load_dave_data():
-    if not os.path.isdir("data/dave") or not os.listdir("data/dave"):
-        build_dave()
+    if not os.path.isdir(os.path.join(DATA_BASE_DIR, "dave")) or not os.listdir(os.path.join(DATA_BASE_DIR, "dave")):
+        build_dave_data()
 
     names = []
     datasets = []
@@ -141,14 +145,14 @@ def load_dave_data():
     meta = []
     lengths = []
 
-    with open("data/data.txt", "r") as f:
+    with open(os.path.join(DATA_BASE_DIR, "data.txt"), "r") as f:
         for i, line in enumerate(f):
             if not i:
                 continue
             igeom,atom1,atom2,r,KELL,S,bo,q1,q2,KEHL = line.strip().split(",")
 
             name = "dave-%04d" % (int(igeom) - 1)
-            path = "data/dave/" + name + ".out"
+            path = os.path.join(DATA_BASE_DIR, "dave", name + ".out")
 
             elements, numbers, coords = read_file_data(path)
 
