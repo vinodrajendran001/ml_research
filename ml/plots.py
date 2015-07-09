8d284b377e16cdb0ea77baf56983c8133969e9d7
Christopher Collins <crcollins@cmu.edu>
2015-07-08 22:33:48 -0400

    Add code for doing 3d plots

--- ml/plots.py
+++ ml/plots.py
@@ -1,6 +1,9 @@
+from matplotlib import cm
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy
 from scipy.optimize import curve_fit
+from scipy.interpolate import griddata
 
 
 def get_histogram_plot(property_name, values, units, title=""):
@@ -27,3 +30,35 @@ def get_line_plot(xvals, yvals, title="", fit=None):
 
 def get_bar_plots(names, values, title=""):
     pass
+
+
+def get_3d_suface(xs, ys, zs, method="linear"):
+    A = numpy.array(zip(xs, ys))
+    X = numpy.linspace(min(xs), max(xs), 100)
+    Y = numpy.linspace(min(ys), max(ys), 100)
+    X, Y = numpy.meshgrid(X, Y)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    #ax.scatter(xs, ys, zs)
+    Z = griddata(A, zs, (X, Y), method=method)
+
+    ax.plot_surface(X, Y, Z, #rstride=1, cstride=1,# cmap=cm.coolwarm,
+            linewidth=0, antialiased=False)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+def get_3d_scatter(xs, ys, zs):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xs, ys, zs)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
